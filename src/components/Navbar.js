d7086cadc40a2afeb39038119cb558ca12ff646d
Aman <amanyadav794172@gmail.com>
2022-09-21 17:36:57 +0530

    fixed issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -23,41 +26,60 @@ function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link " aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
+              </Link>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          {/*    <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button className="btn btn-primary" type="submit">
               Search
             </button>
-          </form>
+          </form>*/}
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={props.toggleMode}
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable DarkMode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
-    )
+  );
 }
 
-Navbar.propTypes={
-    title:PropTypes.string.isRequired,
-    aboutText:PropTypes.string.isRequired
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
 
-Navbar.defaultProps={
-    title:"Set title here",
-    aboutText:"About text here"
-}
+Navbar.defaultProps = {
+  title: "Set title here",
+  aboutText: "About ",
+};
 
-export default Navbar
+export default Navbar;
